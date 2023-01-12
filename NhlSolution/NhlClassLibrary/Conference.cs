622a6d5d8bc7510af48e7d6e5d100bffa09bcd32
John SABANDO <jsabando1@nait.ca>
2023-01-12 10:27:06 -0700

    Added conference, division, team class

---
+++ NhlSolution/NhlClassLibrary/Conference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlClassLibrary
+{
+    public enum Conference
+    {
+        Eastern,
+        Western
+    }
+}
---
+++ NhlSolution/NhlClassLibrary/Team.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlClassLibrary
+{
+    public class Team
+    {
+        // Define fully implemented properties with a backing field for Name, City, Arena, Conference, Division
+        private string _name;
+        private string _city;
+        private string _arena;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                //Validate new value is not blank and contains only letters a-z
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(Name), "Name cannot be blank.");
+                }
+                _name = value.Trim();
+            }
+        }
+        // Define auto-implemented properties for: Conference, Division
+
+        // Greedy constructor
+        public Team(string name)
+        {
+            Name = name;
+        }
+    }
+}
